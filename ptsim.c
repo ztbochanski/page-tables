4b9effd69b0d6a8512d2a2b0b5d087b6cc54e0ae
zach <ztbochanski@gmail.com>
2022-03-02 18:37:10 -0800

    add initialization of mem array

--- ptsim.c
+++ ptsim.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <assert.h>
 
-#define MEM_SIZE 16384  // MUST equal PAGE_SIZE * PAGE_COUNT
+#define MEM_SIZE 16384 // MUST equal PAGE_SIZE * PAGE_COUNT
 #define PAGE_SIZE 256  // MUST equal 2^PAGE_SHIFT
 #define PAGE_COUNT 64
-#define PAGE_SHIFT 8  // Shift page number this much
+#define PAGE_SHIFT 8 // Shift page number this much
 
 // Simulated RAM
 unsigned char mem[MEM_SIZE];
@@ -24,7 +24,13 @@ int get_address(int page, int offset)
 //
 void initialize_mem(void)
 {
-    // TODO
+    for (size_t i = 0; i < MEM_SIZE; ++i)
+    {
+        mem[i] = 0;
+    }
+
+    mem[0] = 1;
+    printf("%d\n", mem[0]);
 }
 
 //
@@ -54,10 +60,11 @@ void print_page_free_map(void)
 {
     printf("--- PAGE FREE MAP ---\n");
 
-    for (int i = 0; i < 64; i++) {
+    for (int i = 0; i < 64; i++)
+    {
         int addr = get_address(0, i);
 
-        printf("%c", mem[addr] == 0? '.': '#');
+        printf("%c", mem[addr] == 0 ? '.' : '#');
 
         if ((i + 1) % 16 == 0)
             putchar('\n');
@@ -67,24 +74,26 @@ void print_page_free_map(void)
 //
 // Print the address map from virtual pages to physical
 //
-void print_page_table(int proc_num)
-{
-    printf("--- PROCESS %d PAGE TABLE ---\n", proc_num);
+// void print_page_table(int proc_num)
+// {
+//     printf("--- PROCESS %d PAGE TABLE ---\n", proc_num);
 
-    // Get the page table for this process
-    int page_table = get_page_table(proc_num);
+//     // Get the page table for this process
+//     int page_table = get_page_table(proc_num);
 
-    // Loop through, printing out used pointers
-    for (int i = 0; i < PAGE_COUNT; i++) {
-        int addr = get_address(page_table, i);
+//     // Loop through, printing out used pointers
+//     for (int i = 0; i < PAGE_COUNT; i++)
+//     {
+//         int addr = get_address(page_table, i);
 
-        int page = mem[addr];
+//         int page = mem[addr];
 
-        if (page != 0) {
-            printf("%02x -> %02x\n", i, page);
-        }
-    }
-}
+//         if (page != 0)
+//         {
+//             printf("%02x -> %02x\n", i, page);
+//         }
+//     }
+// }
 
 //
 // Main -- process command line
@@ -93,25 +102,30 @@ int main(int argc, char *argv[])
 {
     assert(PAGE_COUNT * PAGE_SIZE == MEM_SIZE);
 
-    if (argc == 1) {
+    if (argc == 1)
+    {
         fprintf(stderr, "usage: ptsim commands\n");
         return 1;
     }
-    
+
     initialize_mem();
 
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "np") == 0) {
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "np") == 0)
+        {
             int proc_num = atoi(argv[++i]);
             int pages = atoi(argv[++i]);
             new_process(proc_num, pages);
         }
-        else if (strcmp(argv[i], "pfm") == 0) {
+        else if (strcmp(argv[i], "pfm") == 0)
+        {
             print_page_free_map();
         }
-        else if (strcmp(argv[i], "ppt") == 0) {
+        else if (strcmp(argv[i], "ppt") == 0)
+        {
             int proc_num = atoi(argv[++i]);
-            print_page_table(proc_num);
+            // print_page_table(proc_num);
         }
     }
 }
