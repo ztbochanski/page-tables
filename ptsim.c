3d45754377779c8d2c3497e30ee3b427e2f9894d
zach <ztbochanski@gmail.com>
2022-03-03 20:16:34 -0800

    remove debugging print statement in new_process

--- ptsim.c
+++ ptsim.c
@@ -62,7 +62,6 @@ void new_process(int proc_num, int page_count)
     mem[64 + proc_num] = page_table_number;
     for (int i = 0; i < page_count; ++i)
     {
-        printf("%d", i);
         unsigned char new_data_page = get_page();
         int pt_addr = get_address(page_table_number, i);
         mem[pt_addr] = new_data_page;
